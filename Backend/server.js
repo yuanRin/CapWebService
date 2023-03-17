0a73ca0c3a98ebad26020ca22475c5d0b88ba96f
yuanRin <122252316+yuanRin@users.noreply.github.com>
2023-03-17 09:39:31 +0800

    retry

--- Backend/server.js
+++ Backend/server.js
@@ -1,36 +1,33 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const { Pool } = require('pg');
-
-const app = express();
-
-app.use(bodyParser.json());
-app.use(cors());
-
-const port = process.env.PORT || 443; // Use assigned port or default to 3000
-app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = require("express");
+var body_parser_1 = require("body-parser");
+var cors_1 = require("cors");
+var pg_1 = require("pg");
+var app = (0, express_1.default)();
+//const port = process.env.PORT || 3000;
+var port = 443;
+app.use(body_parser_1.default.json());
+app.use((0, cors_1.default)());
+app.listen(port, function () {
+    console.log("Server started on port ".concat(port));
 });
-
-const pool = new Pool({
+var pool = new pg_1.Pool({
     user: 'testcap_user',
     host: 'dpg-cg3ufmik728m6o3evo30-a.singapore-postgres.render.com',
     database: 'testcap',
     password: 'iymVSpqGphIx0ww1LZxAdO34EFJE3HuI',
     port: 5432,
-    ssl: true, // enable SSL/TLS
-    connectionTimeoutMillis: 10000 // Increase the timeout value to 5 seconds
+    ssl: true,
+    connectionTimeoutMillis: 10000 // Increase the timeout value to 10 seconds
 });
-
-app.get('/users', (req, res) => {
-    pool.query('SELECT uid, frequency, voltage FROM public.users;', (err, result) => {
+app.get('/users', function (req, res) {
+    pool.query('SELECT uid, frequency, voltage FROM public.users;', function (err, result) {
         if (err) {
             console.error(err);
             res.status(500).json({ error: 'Error retrieving users from database' });
             return;
         }
         res.status(200).json(result.rows);
-        res.end;
     });
 });
---
+++ Backend/server.ts
@@ -0,0 +1,36 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import { Pool } from 'pg';
+
+const app = express();
+//const port = process.env.PORT || 3000;
+const port = 443;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
+
+const pool = new Pool({
+  user: 'testcap_user',
+  host: 'dpg-cg3ufmik728m6o3evo30-a.singapore-postgres.render.com',
+  database: 'testcap',
+  password: 'iymVSpqGphIx0ww1LZxAdO34EFJE3HuI',
+  port: 5432,
+  ssl: true, // enable SSL/TLS
+  connectionTimeoutMillis: 10000 // Increase the timeout value to 10 seconds
+});
+
+app.get('/users', (req, res) => {
+  pool.query('SELECT uid, frequency, voltage FROM public.users;', (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Error retrieving users from database' });
+      return;
+    }
+    res.status(200).json(result.rows);
+  });
+});
