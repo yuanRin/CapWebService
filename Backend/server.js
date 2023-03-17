5b62494f45e96bf2384a1de870284636ae899a3c
yuanRin <122252316+yuanRin@users.noreply.github.com>
2023-03-17 09:10:58 +0800

    :d

--- Backend/server.js
+++ Backend/server.js
@@ -8,8 +8,9 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.listen(3000, () => {
-    console.log('Server started on port 3000');
+const port = process.env.PORT || 3000; // Use assigned port or default to 3000
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
 });
 
 const pool = new Pool({
